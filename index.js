8162fe7d4dc96aead01fb6cc51f10ce7e2c18c7a
kush Muhamad <kushmohammed98@yahoo.com>
2023-03-28 17:25:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = { name, email };
+  
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObj)
+      .then(response => response.json())
+      .then(data => {
+        const id = data.id;
+        document.body.innerHTML += id;
+      })
+      .catch(error => {
+        document.body.innerHTML += error.message;
+      });
+  }
+  const name = "John Doe";
+const email = "johndoe@example.com";
+const data = { name, email };
+
+fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(data)
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error))
